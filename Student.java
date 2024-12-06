dbc292e09ffb125d087b8a56b7fbfb87f9a0c99a
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-12-06 14:40:49 +0000

    declared student class

---
+++ Student.java
@@ -0,0 +1,18 @@
+/*Example CLASS that defines an OBJECT
+Class names lilke Student are always captialized 
+Object-Oriented Classes are like BLUEPRINTS
+(in other java class, you creat istance from this blueprint)
+*/
+public class Student {
+    //1. INSTANCE VARIABLES (attributes/data)
+    // think of what this object "needs to know"
+    String name; // "a Studnet has a name"
+    int gradYear;
+
+    //2. CONSTRUCTORS (manuals for setting up an object)
+    //3. MEHTODS (behvaiors associated with the object type)
+
+    public static void Student(String []args) {
+       System.out.println("Hello World");
+    }
+ }
