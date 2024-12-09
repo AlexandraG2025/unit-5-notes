aef1be18bc67b8422aa8b986836e4296e99e9d15
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-12-09 14:25:59 +0000

    private

--- Student.java
+++ Student.java
@@ -4,10 +4,15 @@ Object-Oriented Classes are like BLUEPRINTS
 (in other java class, you creat istance from this blueprint)
 */
 public class Student {
-    //1. INSTANCE VARIABLES (attributes/data)
+    //1. INSTANCE VARIABLES (attributes/data/fields)
     // think of what this object "needs to know"
-    String name; // "a Studnet has a name"
-    int gradYear;
+    // must be PRIVATE ("encapsualtion")
+    private String name; // "a Studnet has a name"
+    private int gradYear;
+    private String email;
+    private boolean sports;
+    private double gpa;
+
 
     //2. CONSTRUCTORS (manuals for setting up an object)
     //3. MEHTODS (behvaiors associated with the object type)
